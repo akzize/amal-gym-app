215a8e9be3b360f2764e4d6a99b32437c57b405e
mrakzize <akziz.bizdev@gmail.com>
2025-11-18 11:52:52 +0100

    add the trainee card design

---
+++ resources/views/cards/trainee.blade.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        @vite('resources/css/app.css')
+        <style>
+            /* Card Container */
+            .gym-card {
+                width: 450px;
+                background: #0d0d0f;
+                color: #ffffff;
+                border-radius: 18px;
+                padding: 18px;
+                font-family: "Segoe UI", Arial, sans-serif;
+                display: flex;
+                flex-direction: column;
+                gap: 18px;
+                box-shadow: 0 4px 12px rgba(0, 0, 0, 0.35);
+                aspect-ratio: 16/10;
+            }
+
+            /* Top Section */
+            .card-header {
+                display: flex;
+                align-items: center;
+                gap: 10px;
+            }
+
+            .logo {
+                width: 45px;
+                height: 45px;
+                /* background: #d50000; */
+                border-radius: 50%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-weight: bold;
+            }
+
+            .card-header h1 {
+                font-size: 16px;
+                letter-spacing: 1px;
+                font-weight: 700;
+            }
+
+            /* Middle Section */
+            .card-body {
+                display: flex;
+                gap: 15px;
+            }
+
+            .member-photo {
+                width: 120px;
+                height: 140px;
+                object-fit: cover;
+                border-radius: 10px;
+                border: 2px solid #d50000;
+            }
+
+            .member-info {
+                display: flex;
+                flex-direction: column;
+                gap: 6px;
+                width: calc(100% - 120px);
+            }
+
+            /* Info Blocks */
+            .info-block label {
+                font-size: 10px;
+                text-transform: uppercase;
+                color: #bbbbbb;
+            }
+
+            .info-block p {
+                margin: 0;
+                font-size: 13px;
+                font-weight: 600;
+            }
+
+            /* Bottom Section */
+            .card-footer {
+                display: flex;
+                justify-content: center;
+            }
+
+            .qr-code {
+                /* width: 90px;
+                height: 90px; */
+            }
+        </style>
+    </head>
+
+    <body class="p-25">
+        <div class="gym-card">
+            <!-- Top Section -->
+            <div class="card-header">
+                <div class="logo bg-white">
+                    <img src="{{ asset("images/bitmap1.png") }}" class="" alt="">
+                </div>
+                <div><h2 class="text-gym-text-primary font-bold text-lg tracking-wide leading-tight">AMAL GYM</h2><p class="text-gym-text-secondary text-xs font-medium tracking-wider">OUARZAZATE</p></div>
+            </div>
+
+            <!-- Middle Section -->
+            <div class="card-body">
+                <img src="{{ url($trainee->photo_url) }}"
+                    alt="Member Photo" class="member-photo" />
+
+                <div class="member-info">
+                    <div class="info-block">
+                        {{-- <label>Full Name</label> --}}
+                        <h3 id="fullName" class="text-gym-text-primary mb-2 truncate text-xl font-bold">{{ $trainee->full_name }}</h3>
+                    </div>
+
+                    <div class="info-block">
+                        <label>Membership Type</label>
+                        <p id="membershipType">Monthly Membership</p>
+                    </div>
+
+                    <div class="info-block w-full flex justify-between">
+                        <div>
+                            <label>Member ID</label>
+                            <p id="memberId">ID-00001</p>
+                        </div>
+                        <div>
+                            @php
+                                $qr = tbQuar\Facades\Quar::eye('rounded')->color(235, 12, 83)
+                                ->backgroundColor(13,13,15)
+            ->generate('Quar package create qr code');
+            
+                            @endphp 
+                            {{ $qr }}
+                        </div>
+                    </div>
+
+                    {{-- <div class="info-block">
+                        <label>Access Level</label>
+                        <p id="accessLevel">Full Access</p>
+                    </div> --}}
+                </div>
+            </div>
+
+            <!-- Bottom Section -->
+            {{-- <div class="card-footer"> --}}
+                
+                {{-- <img src="https://via.placeholder.com/100" alt="QR Code" class="qr-code" /> --}}
+            {{-- </div> --}}
+        </div>
+
+    </body>
+
+</html>
