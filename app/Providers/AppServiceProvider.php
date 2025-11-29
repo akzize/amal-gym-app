6a2b720adbb7cb4fa1f816cc39f0505c5eb62d5c
akzize <akzakariae@gmail.com>
2025-11-29 16:56:22 +0100

    register the datepicker to use the filament default one globally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Filament\Forms\Components\DatePicker;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // prevent fron using the native date picker globally
+        DatePicker::configureUsing(function(DatePicker $datePicker){
+            $datePicker->native(false);
+        });
     }
 }
