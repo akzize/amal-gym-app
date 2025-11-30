1ea879171cc8ab5805d907c8f3d6bcd0546b35e6
akzize <akzakariae@gmail.com>
2025-11-30 14:08:42 +0100

    refactor the trainer form abd fix disabled fields in create action

--- app/Filament/Resources/Trainers/Schemas/TrainerForm.php
+++ app/Filament/Resources/Trainers/Schemas/TrainerForm.php
@@ -9,6 +9,7 @@ use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
 
 class TrainerForm
@@ -30,25 +31,25 @@ class TrainerForm
                         Hidden::make('user_id'),
                         TextInput::make('name')
                             ->label(__('resources.trainer.name'))
-                            ->disabled(fn(callable $get) => !$get('edit_login'))
-                            ->dehydrated(fn(callable $get) => $get('edit_login'))
-                            ->required(fn(callable $get) => $get('edit_login')),
+                            ->disabled(fn(callable $get, string $operation) => ($operation == 'edit' && !$get('edit_login')))
+                            ->dehydrated(fn(callable $get, string $operation) => $get('edit_login') || $operation == 'create')
+                            ->required(fn(callable $get, string $operation) => $get('edit_login') || $operation == 'create'),
                         TextInput::make('user.email')
                             ->label(__('resources.trainer.email'))
-                            ->disabled(fn(callable $get) => !$get('edit_login'))
-                            ->dehydrated(fn(callable $get) => $get('edit_login'))
+                            ->disabled(fn(callable $get, string $operation) => ($operation == 'edit' && !$get('edit_login')))
+                            ->dehydrated(fn(callable $get, string $operation) => $get('edit_login') || $operation == 'create')
                             ->email()
-                            ->required(fn(callable $get) => $get('edit_login'))
+                            ->required(fn(callable $get, string $operation) => $get('edit_login') || $operation == 'create')
                             ->unique(User::class, 'email', fn($record) => $record?->user),
 
                         TextInput::make('user.password')
                             ->label(__('resources.trainer.password'))
-                            ->disabled(fn(callable $get) => !$get('edit_login'))
-                            ->dehydrated(fn(callable $get) => $get('edit_login'))
+                            ->disabled(fn(callable $get, string $operation) => ($operation == 'edit' && !$get('edit_login')))
+                            ->dehydrated(fn(callable $get, string $operation) => $get('edit_login') || $operation == 'create')
                             ->password()
                             ->revealable(true)
                             ->copyable(copyMessage: 'Copied!', copyMessageDuration: 1500)
-                            ->required(fn(callable $get) => $get('edit_login')),
+                            ->required(fn(callable $get, string $operation) => $get('edit_login') || $operation == 'create'),
                     ]),
 
                 Section::make(__('resources.trainer.Payment info'))
@@ -56,17 +57,30 @@ class TrainerForm
                         // 'salary_type' label will be automatically translated
                         Select::make('salary_type')
                             ->label(__('resources.trainer.salary_type'))
-
+                            ->reactive()
                             ->options([
-                                'fixed' => __('resources.trainer.fixed'),         // Added __()
-                                'per_group' => __('resources.trainer.per_group'),   // Added __()
-                                'per_trainee' => __('resources.trainer.per_trainee'), // Added __()
-                                'none' => __('resources.trainer.none'),           // Added __()
+                                'fixed' => __('resources.trainer.fixed'),
+                                'percentage' => __('resources.trainer.percentage'),
                             ])
                             ->required(),
                         // 'salary_amount' label will be automatically translated
                         TextInput::make('salary_amount')
-                            ->label(__('resources.trainer.salary_amount'))
+                            // ->label(__('resources.trainer.salary_amount'))
+                            ->label(function (Get $get) {
+                                $type = $get('salary_type');
+
+                                if ($type == 'percentage') {
+                                    return __('resources.trainer.payment_percent');
+                                }
+
+                                return __('resources.trainer.salary_amount');
+                            })
+                            ->visible(fn(Get $get): bool => $get('salary_type') !== 'none')
+                            ->suffix(function(Get $get){
+                                $type = $get('salary_type');
+                                return $type == 'percentage' ? '%' : 'MAD';
+                            })
+                            ->reactive()
                             ->numeric(),
 
                         // 'status' label will be automatically translated
