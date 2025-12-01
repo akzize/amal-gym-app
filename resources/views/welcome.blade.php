f75a9127d74511a6fa111f86b7d9b2662492db82
akzize <akzakariae@gmail.com>
2025-12-01 22:41:22 +0100

    add the site favicons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,13 @@
         <meta name="author" content="Amal Gym Ouarzazate">
         <meta name="robots" content="index, follow">
 
+        {{-- favicons --}}
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+
         <!-- Local Business GEO Tags -->
         <meta name="geo.region" content="MA-OUA">
         <meta name="geo.placename" content="Ouarzazate, Hay Fedragoum">
@@ -57,7 +64,7 @@
             "name": "Amal Gym Ouarzazate",
             "alternateName": "نادي أمل للرياضة",
             "image": "https://amalgymouarzazate.com/images/amal-gym-cover.png",
-            "logo": "https://amalgymouarzazate.com/images/logo.png",
+            "logo": "https://amalgymouarzazate.com/images/amal-gym-logo.png",
             "url": "https://amalgymouarzazate.com",
             "telephone": "+212624836434",
             "priceRange": "$$",
